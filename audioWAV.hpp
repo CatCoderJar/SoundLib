9af1489c79d0edcf10299cf4547ba2a07e3d82e5
CatCoderJar <spaceredstoneminecrafted@gmail.com>
2026-07-12 13:05:02 +0300

    Refactor AudioRecorder and AudioPlayer classes

--- audioWAV.hpp
+++ audioWAV.hpp
@@ -5,7 +5,6 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
-#pragma comment(lib, "ole32.lib")
 
 #define SAFE_RELEASE(punk) \
     if ((punk) != nullptr) \
@@ -14,32 +13,13 @@
         (punk) = nullptr;  \
     }
 
-struct WAVHeader
-{
-	char riff[4] = { 'R','I','F','F' };
-	uint32_t fileSize;
-
-	char wave[4] = { 'W','A','V','E' };
-
-	char fmt[4] = { 'f','m','t',' ' };
-	uint32_t fmtSize = 16;
-
-	uint16_t audioFormat = 3;
-	uint16_t channels;
-	uint32_t sampleRate;
-	uint32_t byteRate;
-	uint16_t blockAlign;
-	uint16_t bitsPerSample;
-
-	char data[4] = { 'd','a','t','a' };
-	uint32_t dataSize;
-};
-
 class AudioRecorder
 {
 private:
 	HRESULT hr;
 
+	UINT32 frames = 0;
+	BYTE* pData = nullptr;
 	IMMDeviceEnumerator* pEnumerator = nullptr;
 	IMMDevice* pDevice = nullptr;
 	IAudioClient* pAudioClient = nullptr;
@@ -124,7 +104,7 @@ public:
 
 	void initializeHeaderBeforeReturnRecord(WAVHeader headerInit)  // put as a link, initialization for header before audioRecordReturn
 	{
-		headerInit.audioFormat = 1;
+		headerInit.audioFormat = pwfx->wFormatTag;
 		headerInit.channels = pwfx->nChannels;
 		headerInit.sampleRate = pwfx->nSamplesPerSec;
 		headerInit.bitsPerSample = pwfx->wBitsPerSample;
@@ -135,10 +115,28 @@ public:
 		headerInit.fileSize = 36;
 	}
 
-	void recordStart() // use this before audioRecordReturn() 
+	BYTE* audioRecordReturn() // Do in cycle, can be sent by tcp client, call releasePData() after every use
 	{
-		pAudioClient->Start();
+		UINT32 bytes = 0;
+		DWORD flags = 0;
+		UINT32 packetLenght{ 0 };
+
+		packetSize:
+		pCapture->GetNextPacketSize(&packetLenght);
+
+		if (packetLenght == 0)
+		{
+			Sleep(5);
+			goto packetSize;
+		}
+		HRESULT hr = pCapture->GetBuffer(&pData, &frames, &flags, nullptr, nullptr);
+
+		bytes = frames * pwfx->nBlockAlign;
+
+		return pData;
 	}
+
+	void releasePData() { pCapture->ReleaseBuffer(frames); }
 
 	BYTE* audioRecordReturn(WAVHeader headerArg) // Do in cycle, can be sent by tcp client, put WAVHeader as &(link) in second argument, then make file.seekp(0) and file.write(reinterpret_cast<const char*>(yourHeader), sizeof(yourHeader);
 	{
@@ -146,6 +144,16 @@ public:
 		UINT32 frames = 0;
 		UINT32 bytes = 0;
 		DWORD flags = 0;
+		UINT32 packetLenght{ 0 };
+
+	packetSize:
+		pCapture->GetNextPacketSize(&packetLenght);
+
+		if (packetLenght == 0)
+		{
+			Sleep(5);
+			goto packetSize;
+		}
 
 		do
 		{
@@ -154,24 +162,6 @@ public:
 			bytes = frames * pwfx->nBlockAlign;
 
 			headerArg.dataSize += bytes;
-			pCapture->ReleaseBuffer(frames);
-		} while (FAILED(hr));
-		return pData;
-	}
-
-	BYTE* audioRecordReturn() // Do in cycle, can be sent by tcp client, put WAVHeader as &(link) in second argument, then make file.seekp(0) and file.write(reinterpret_cast<const char*>(yourHeader), sizeof(yourHeader);
-	{
-		BYTE* pData = nullptr;
-		UINT32 frames = 0;
-		UINT32 bytes = 0;
-		DWORD flags = 0;
-
-		do
-		{
-			HRESULT hr = pCapture->GetBuffer(&pData, &frames, &flags, nullptr, nullptr);
-
-			bytes = frames * pwfx->nBlockAlign;
-
 			pCapture->ReleaseBuffer(frames);
 		} while (FAILED(hr));
 		return pData;
@@ -270,8 +260,25 @@ public:
 		}
 		return 0;
 	}
-	void stopPlay()
-	{
-		pAudioClient->Stop();
-	}
+};
+
+struct WAVHeader
+{
+	char riff[4] = { 'R','I','F','F' };
+	uint32_t fileSize;
+
+	char wave[4] = { 'W','A','V','E' };
+
+	char fmt[4] = { 'f','m','t',' ' };
+	uint32_t fmtSize = 16;
+
+	uint16_t audioFormat = 3;
+	uint16_t channels;
+	uint32_t sampleRate;
+	uint32_t byteRate;
+	uint16_t blockAlign;
+	uint16_t bitsPerSample;
+
+	char data[4] = { 'd','a','t','a' };
+	uint32_t dataSize;
 };
